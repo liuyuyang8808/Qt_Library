f169560739fb2755a4c8f76549ba40b82d17fc16
liuyuyang <wb.liuyuyang@mesg.corp.netease.com>
2023-07-26 21:13:20 +0800

    添加鼠标事件

--- Library/UEProgressBar.cpp
+++ Library/UEProgressBar.cpp
@@ -1,11 +1,14 @@
 #include "UEProgressBar.h"
 #include "ui_ueprogressBar.h"
+#include <QDebug>
 
 UEProgressBar::UEProgressBar(QWidget* parent)
 	: QWidget(parent)
 	, ui(new Ui::UEProgressBar)
 {
 	ui->setupUi(this);
+
+	m_progress_bar = ui->progressBar;
 
 	InitAll();
 }
@@ -17,7 +20,53 @@ UEProgressBar::~UEProgressBar()
 
 void UEProgressBar::InitAll()
 {
+	setCursor(Qt::SizeHorCursor);
 }
+
+void UEProgressBar::enterEvent(QEvent* event)
+{
+	qDebug() << "enter event";
+}
+
+void UEProgressBar::leaveEvent(QEvent* event)
+{
+	qDebug() << "leave event";
+}
+
+void UEProgressBar::mousePressEvent(QMouseEvent* event)
+{
+	if (event->button() == Qt::LeftButton)
+	{
+		this->setMouseTracking(true);
+	}
+
+	qDebug() << "mouse press";
+}
+
+void UEProgressBar::mouseReleaseEvent(QMouseEvent* event)
+{
+	this->setMouseTracking(false);
+	setCursor(Qt::SizeHorCursor);
+
+	qDebug() << "mouse release";
+}
+
+void UEProgressBar::mouseMoveEvent(QMouseEvent* event)
+{
+	setCursor(Qt::BlankCursor);
+
+	m_is_mouse_move = true;
+	m_current_mouse_pos = event->pos().x();
+	int delta = m_current_mouse_pos - m_last_mouse_pos;
+	m_current_mouse_pos += delta;
+
+	m_progress_bar->setValue(m_current_mouse_pos);
+
+	
+
+	qDebug() << "mouse move";
+}
+
 
 
 
--- Library/UEProgressBar.h
+++ Library/UEProgressBar.h
@@ -1,6 +1,7 @@
 #include <QWidget>
 #include <QFrame>
 #include <QProgressBar>
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class UEProgressBar; }
@@ -16,8 +17,26 @@ public:
 
 	void InitAll();
 
+protected:
+	void enterEvent(QEvent* event) override;
+	void leaveEvent(QEvent* event) override;
+	void mousePressEvent(QMouseEvent* event) override;
+	void mouseReleaseEvent(QMouseEvent* event) override;
+	void mouseMoveEvent(QMouseEvent* event) override;
 private:
 	Ui::UEProgressBar* ui;
+
+	bool  m_is_mouse_move = false;
+	
+	float m_current_value = 0.0f;
+	float m_max_value = 360.0f;
+	float m_min_value = 0.0f;
+	
+	int m_current_mouse_pos = 0.0f;
+	int m_last_mouse_pos = 0.0f;
+
+	QProgressBar* m_progress_bar = nullptr;
+
 };
 
 
